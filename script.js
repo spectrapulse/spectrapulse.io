ca4f3a1a9b40087222207f6e010869d4ba1284bc
Tyrone Faulhaber <code@spectrapulse.io>
2022-10-30 18:47:19 +0100

    lol

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let deg = 0;
 
 // Animate page border with rotating linear background
 function animateBorder() {
-  html.style.background = `background linear-gradient(${deg++}deg, #ff6600, #029afe)`;
+  deg >= 360 ? deg = 0 : deg++; // Future Proofing :)
+  html.style.background = `background linear-gradient(${deg}deg, #ff6600, #029afe)`;
+
   setTimeout(() => {
     requestAnimationFrame(animateBorder);
   }, 1000 / 48); // 48fps
