bce5168c7fa1eda132369912e8fab6d6fad3cba6
Tyrone Faulhaber <20131658+spectrapulse@users.noreply.github.com>
2024-08-25 22:52:46 +0200

    Create keys.sh

---
+++ keys.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+GITHUB_USER="spectrapulse"
+
+KEYS_URL="http://github.com/${GITHUB_USER}.keys"
+
+SSH_DIR="${HOME}/.ssh"
+AUTHORIZED_KEYS_FILE="${SSH_DIR}/authorized_keys"
+
+# Create '.ssh' dir if it doesn't exist.
+if [ ! -d "${SSH_DIR}" ]; then
+  mkdir -p "${SSH_DIR}"
+  chmod 700 "${SSH_DIR}"
+fi
+
+# Add keys to authorized_keys file if they're not already present
+while IFS= read -r KEY; do
+  if ! grep -q "${KEY}""${AUTHORIZED_KEYS_FILE}"; then
+    echo "$KEY" >> "${AUTHORIZED_KEYS_FILE}"
+    echo "Added new key: ${KEY}"
+  else
+    echo "Key already exists: ${KEY}"
+  fi
+done <<< "$(curl -s "${KEYS_URL}")"
+
+# Set correct permissions for authorized_keys file (Incase the permissions are incorrect or haven't been set because the file was created in the previous step)
+chmod 600 "${AUTHORIZED_KEYS_FILE}"
+
+echo "SSH keys from ${GITHUB_USER} have been processed and added to ${AUTHORIZED_KEYS_FILE} if they were not already present."
+  
