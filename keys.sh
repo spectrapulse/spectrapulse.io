f318af285c2a9f3a9ba8522a0aa899d0e6d2aabb
Tyrone Faulhaber <20131658+spectrapulse@users.noreply.github.com>
2024-08-25 23:22:44 +0200

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -7,9 +7,6 @@ KEYS_URL="http://github.com/${GITHUB_USER}.keys"
 SSH_DIR="${HOME}/.ssh"
 AUTHORIZED_KEYS_FILE="${SSH_DIR}/authorized_keys"
 
-# Shell debug
-set -x
-
 # Create '.ssh' dir if it doesn't exist.
 if [ ! -d "${SSH_DIR}" ]; then
   mkdir -p "${SSH_DIR}"
@@ -21,20 +18,19 @@ if [ ! -f "${AUTHORIZED_KEYS_FILE}" ]; then
   install -m 600 /dev/null "${AUTHORIZED_KEYS_FILE}"
 fi
 
+# Fetch and print keys
+KEYS=$(curl -s $KEYS_URL)
+echo -e "Fetched keys from ${KEYS_URL}:\n${KEYS}"
+
 # Add keys to authorized_keys file if they're not already present
-curl -s $KEYS_URL | while IFS= read -r KEY; do
-  echo "Read key while: ${KEY}"
+for KEY in ${KEYS}; do
   if ! grep -qF "${KEY}" "${AUTHORIZED_KEYS_FILE}"; then
-    echo "Found key grep: ${KEY}"
     echo "$KEY" | tee -a "${AUTHORIZED_KEYS_FILE}" > /dev/null
     echo "Added new key: ${KEY}"
   else
     echo "Key already exists: ${KEY}"
   fi
 done
-
-# Disable shell debug
-set +x
 
 echo "SSH keys from ${GITHUB_USER} have been processed and added to ${AUTHORIZED_KEYS_FILE} if they were not already present."
   
